912390bda7deae40b19028fba1ca49180007043d
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-03 11:08:29 +0200

    ft(Html and css): added html and css design

--- Javascript.js
+++ Javascript.js
@@ -1,39 +1,48 @@
+/** @format */
+
 const item = [
-  { name: "Bike", price: 100 },
-  { name: "TV", price: 200 },
-  { name: "Album", price: 10 },
-  { name: "Book", price: 5 },
-  { name: "Phone", price: 500 },
-  { name: "Computer", price: 1000 },
+	{ name: "Bike", price: 100 },
+	{ name: "TV", price: 200 },
+	{ name: "Album", price: 10 },
+	{ name: "Book", price: 5 },
+	{ name: "Phone", price: 500 },
+	{ name: "Computer", price: 1000 },
 ];
-​
+
 // Question 1
 const cheapestItem = item.reduce((first, second) =>
-  first.price < second.price ? first : second
+	first.price < second.price ? first : second
 );
-console.log("Q1. Without money, I will Buy:");
-console.log(cheapestItem.name + " with price of " + cheapestItem.price);
-​
+console.log(
+	"Q1. Without money, I will Buy a " +
+		cheapestItem.name +
+		" with price of " +
+		cheapestItem.price
+);
+
 // Question 2
 const expensiveItem = item.reduce((first, second) =>
-  first.price > second.price ? first : second
+	first.price > second.price ? first : second
 );
-console.log("Q2. The expensive product is:");
-console.log(expensiveItem.name + " with price of " + expensiveItem.price);
-​
+console.log(
+	"Q2. The expensive product is " +
+		expensiveItem.name +
+		" with price of " +
+		expensiveItem.price
+);
+
 // Question 3
 let total = 0;
 item.forEach((values) => {
-  total += values.price;
+	total += values.price;
 });
 console.log("Q3. The total price of all products is " + total);
-​
+
 // Question 4
 let total2 = 0;
 item.forEach((values) => {
-    if (values.price >= 10) {
-        total2 += values.price;
-        
-  }
+	if (values.price >= 10) {
+		total2 += values.price;
+	}
 });
 console.log("Q4. The total without whose price is < 10 is " + total2);
